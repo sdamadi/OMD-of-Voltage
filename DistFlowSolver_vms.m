aa4ae1f9628800b7db4d86419a6a2b8fa3aa76c5
sdamadi <sdamadi1@umbc.edu>
2018-04-26 12:07:13 -0400

    Adding capability of changing Q matrix

---
+++ DistFlowSolver_vms.m
@@ -0,0 +1,36 @@
+function [c,P, Q,l,vms,y2,y4] = DistFlowSolver_vms(nbr,n,r,x,p_c,q_c,p_g,q_g,children,injection_matrix,parent,PV_matrix,PV_n,eps1,eps2,var)
+cvx_begin quiet
+
+variables  P(nbr) Q(nbr) l(nbr) vms(n) %q_g(PV_n)  
+dual variables y1 y2 y3 y4 y5 y6{nbr} y7
+      
+c=r'*l;
+      minimize c
+      
+        y1 :      P    == children*P + diag(r)*l    +  injection_matrix*( var*eps1 + p_c - PV_matrix*p_g )
+        
+                %       P_12 == P_23       + r(1)*l_12    +  p_c_2
+                %       P_23 ==              r(2)*l_23    +  p_c_3
+      
+        y2 :      Q    == children*Q + diag(x)*l    +  injection_matrix*( var*eps2 + q_c - PV_matrix*q_g ) %
+          
+        y3 :    vms(1) == 1
+            
+        y4 : injection_matrix*vms == parent*vms - 2*( diag(r)*P + diag(x)*Q ) + ( diag(r.^2) + diag(x.^2) )*l     
+              
+%         y5 : PV_matrix'*vms == ones(PV_n,1)
+
+for i=1:nbr
+
+    norm ( [ 2*P(i) 2*Q(i) ( l(i) - vms(i) ) ] ) - ( l(i) + vms(i) ) <= 0 : y6{i}
+    
+end
+
+
+
+        y7: (1-0.05)^2*ones(n-1,1)<=[zeros(n-1,1) eye(n-1,n-1)]*vms<=(1+0.05)^2*ones(n-1,1)
+        
+        %(1-0.05)^2*ones(PQ_n,1)<=PQ_matrix*vms<=(1+0.05)^2*ones(PQ_n,1)
+%            -q_g_max<=q_g<=q_g_max
+ 
+ cvx_end
--- OMD_Voltage.m
+++ OMD_Voltage.m
@@ -322,16 +322,12 @@ p_g_nom = PV_ratio*p_g_max;
 % ------------ % Maximum reactive power produced by inverters of PVs % ------------ % 
 q_g_max = 0.45*p_g_max;
 q_g_min = -q_g_max;
-% ------------ % Number of periods % ------------ %
-T = 361;
-% ------------ % Number of realization of COMID % ------------ %
-num_real = 1;
 % ------------ % The variance of changing loads and PVs % ------------ %
-var_1 = 0.12;
+var_1 = 0.08;
 var_2 = 0.00;
 % ------------ % The OMD's parameter % ------------ %
-eta_q = .5;
-eta_sig = .5;
+eta_q = 1;
+eta_sig = 1;
 c_til_0 = 6.6*1000;
 c_n = 1/80;
 c_til_n = 6.6*1000*c_n;
@@ -360,6 +356,11 @@ C = [1,ep12,ep13,ep14,ep15,ep16;
      ep16,ep26,ep36,ep46,ep56,1];
 Q = inv(C);
  
+% ------------ % Number of periods % ------------ %
+T = 61;
+% ------------ % Number of realization of COMID % ------------ %
+num_real = 1;
+% ------------ ------------ ------------ ------------ ------------ %
 
 % ------------ % Preallocation of vectors to avoid wasting time % ------------ %
 c0 = zeros(num_real,T);
@@ -378,9 +379,14 @@ p_g_rand = zeros(PV_n,1,T);
 
 % ------------ % Initial values for running COMID % ------------ % 
 q_g = zeros(PV_n,1);
+q_g1 = zeros(PV_n,1);
+q_g2 = zeros(PV_n,1);
 sigma_min_0 = 0;
 sigma_min = sigma_min_0;
+sigma_min1 = sigma_min_0;
 % ------------ ------------ ------------ ------------ ------------ %
+
+
 
 % ------------ % Stochastic OMD for voltage stability % ------------ %
 % ------------ % Stochastic OMD for voltage stability% ------------ %
@@ -392,6 +398,8 @@ sigma_min = sigma_min_0;
 
 q_g_max = 0.45*p_g_nom;
 q_g_min = -p_g_nom;
+
+
 
 
 for o = 1:num_real
@@ -408,23 +416,35 @@ p_g_rand(:,:,k) = p_g_nom + var_2*randn(PV_n,1);
 % ------------ % Keep track of sigma_min evolution % ------------ %
 
 sigma_min_online(k,1) = sigma_min;
+sigma_min_online1(k,1) = sigma_min1;
 
 % ------------ ------------ ------------ ------------ ------------ %
 
 
-% ------------ % The real loss of network (c0) without noise % ------------ %
+% ------------ % The real loss of network (c0) without noise with Lambda % ------------ %
 
 [c0(o,k),P_r,Q_r,l_r,vms_r,~,~,~] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g,children,injection_matrix,parent,PV_matrix,PV_n,lambda,zeros(n,1),zeros(n,1),0);
+[c01(o,k),P_r1,Q_r1,l_r1,vms_r1,~,~,~] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g1,children,injection_matrix,parent,PV_matrix,PV_n,lambda,zeros(n,1),zeros(n,1),0);
 
-% ------------ % Find the voltage pahses and current % ------------ %
+
+% ------------ % The real loss of network (c0) without noise without Lambda % ------------ %
+
+[c02(o,k),P_r2, Q_r2,l_r2,vms_r2] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g2,children,injection_matrix,parent,PV_matrix,PV_n,lambda,zeros(n,1),zeros(n,1),0);
+
+% ------------ % Find the voltage pahses and current with Lambda% ------------ %
 
 [~,delta,I_line] = PhaseRecovery_I(n,nbr,nf,nt,r,x,P_r,Q_r);
+[~,delta1,I_line1] = PhaseRecovery_I(n,nbr,nf,nt,r,x,P_r1,Q_r1);
+
+% ------------ % Find the voltage pahses and current without Lambda% ------------ %
+
+[~,delta2,I_line2] = PhaseRecovery_I(n,nbr,nf,nt,r,x,P_r2,Q_r2);
 
 % ------------ % Check the magnitude of current is the same as DistFlow % ------------ %
 
 I_check = ( abs(I_line) ).^2 - l_r; 
 
-% ------------ % Find the jacobian matrix and real minimum singular value of system % ------------ %
+% ------------ % Find the jacobian matrix and real minimum singular value of system with Lambda % ------------ %
 
 cd 'C:\Users\Saeed\OneDrive\Matpower\matpower6.0'
 
@@ -435,57 +455,104 @@ mpc.bus(:,9) = delta(2:end);
 jac = full(makeJac(mpc));
 sigma_min_act(k,1) = min(svd(jac));
 
+
+filename='case47';
+mpc=loadcase(filename);
+mpc.bus(:,8) = sqrt(vms_r1(2:end));
+mpc.bus(:,9) = delta1(2:end);
+jac = full(makeJac(mpc));
+sigma_min_act1(k,1) = min(svd(jac));
+
+% ------------ % Find the jacobian matrix and real minimum singular value of system without Lambda % ------------ %
+
+filename='case47';
+mpc=loadcase(filename);
+mpc.bus(:,8) = sqrt(vms_r2(2:end));
+mpc.bus(:,9) = delta2(2:end);
+jac = full(makeJac(mpc));
+sigma_min_act2(k,1) = min(svd(jac));
+
+
 cd 'C:\Users\Saeed\OneDrive\UMBC\Dr. Kim\My papers\Matlab\First Paper\OMD-of-Voltage'
 
 % ------------ ------------ ------------ ------------ ------------ %
 
 % ------------ % Solve the problem with stochastic loads % ------------ %
-% ------------ % Finding the dual variables y8_s corresponding to sigma_min from the dual problem with noise % ------------ %
+% ------------ % Finding the dual variables y8_s corresponding to sigma_min from the dual problem with noise with Lambda % ------------ %
 
 eps1(:,k) = randn(n,1);%zeros(n,1);
 eps2(:,k) = randn(n,1);%zeros(n,1);
 
 [c1(o,k),P_s,Q_s,l_s,vms_s,~,y2_s(:,k),~] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g,children,injection_matrix,parent,PV_matrix,PV_n,lambda,eps1(:,k),eps2(:,k),var_1);
+[c11(o,k),P_s1,Q_s1,l_s1,vms_s1,~,y2_s1(:,k),~] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g1,children,injection_matrix,parent,PV_matrix,PV_n,lambda,eps1(:,k),eps2(:,k),var_1);
 
 
-% ------------ % Cost regarding real loss c0 and q_g's achieved from OMD % ------------ %
+% ------------ % Finding the dual variables y8_s corresponding to sigma_min from the dual problem with noise without Lambda % ------------ %
 
-f1(k,1) = c_til_0*c0(o,k) + c_til_n*sum(abs(q_g)) - lambda*sigma_min;   
+[c12(o,k),P_s2,Q_s2,l_s2,vms_s2,~,y2_s2(:,k),~] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g_rand(:,:,k),q_g2,children,injection_matrix,parent,PV_matrix,PV_n,lambda,eps1(:,k),eps2(:,k),var_1);
 
-% ------------ % The gradient of q_g (g_q) % ------------ %
+
+% ------------ % Cost regarding real loss c0 and q_g's achieved from OMD with Lambda % ------------ %
+
+f1(k,1) = c_til_0*c0(o,k) - lambda*sigma_min;   % c_til_0*c0(o,k) + c_til_n*sum(abs(q_g)) - lambda*sigma_min;  
+f11(k,1) = c_til_0*c01(o,k) - lambda*sigma_min1;
+
+% ------------ % Cost regarding real loss c0 and q_g's achieved from OMD without Lambda % ------------ %
+
+f12(k,1) = c_til_0*c02(o,k);
+
+% ------------ % The gradient of q_g (g_q) with Lambda % ------------ %
 g_q = -y2_s([dual_indeces],k);
+g_q1 = -y2_s1([dual_indeces],k);
+
+% ------------ % The gradient of q_g (g_q) without Lambda % ------------ %
+g_q2 = -y2_s2([dual_indeces],k);
 
 % ------------ % The gradient of sigma (g_sig) % ------------ %
 
 if sigma_min_act(k,1) <= sigma_min  
-    g_sig = .1;
+    g_sig = 1;
 elseif sigma_min_act(k,1) > sigma_min
-    g_sig = -.5;
+    g_sig = -1;
 else
 end
 
-% ------------ % Finding q_g for the next step % ------------ %
 
-% y_s_q = q_g - eta_q*inv(Q)*g_q;
+if sigma_min_act1(k,1) <= sigma_min1  
+    g_sig1 = 1;
+elseif sigma_min_act1(k,1) > sigma_min1
+    g_sig1 = -1;
+else
+end
 
-a = C*[g_q; g_sig];
-q_g - eta_q*a(1:5)
+% ------------ % Finding q_g for the next step with Lambda % ------------ %
+
+C = inv(Q);
+
+q_t = COMID_Controller(C,g_q,g_sig,q_g,PV_n,q_g_max,q_g_min,eta_q);
+
+C = eye(size(Q));
+
+q_t1 = COMID_Controller(C,g_q1,g_sig1,q_g1,PV_n,q_g_max,q_g_min,eta_q);
+
+
+
+% ------------ % Finding q_g2 for the next step without Lambda % ------------ %
 
 for i=1:PV_n
     
         
-if  q_g_max(i) <= q_g(i) - eta_q*a(i)
-    q_t(i) = q_g_max(i);
+if  q_g_max(i) <= q_g2(i) - eta_q*g_q2(i)
+    q_t2(i) = q_g_max(i);
    
-    elseif q_g_max(i) > q_g(i) - eta_q*a(i) && q_g_min(i)< q_g(i) - eta_q*a(i)
-    q_t(i) = q_g(i) - eta_q*a(i);
+    elseif q_g_max(i) > q_g2(i) - eta_q*g_q2(i) && q_g_min(i)< q_g2(i) - eta_q*g_q2(i)
+    q_t2(i) = q_g2(i) - eta_q*g_q2(i);
     
-    elseif q_g_min(i) >= q_g(i) - eta_q*a(i)
-    q_t(i) = q_g_min(i);    
+    elseif q_g_min(i) >= q_g2(i) - eta_q*g_q2(i)
+    q_t2(i) = q_g_min(i);    
           
 end
 
-q_t
 
 end
 
@@ -493,6 +560,8 @@ end
 
 % ------------ % Finding sigma_min for the next step % ------------ %
 
+C = inv(Q);
+a = C*[g_q; g_sig];
 
 if  0 < sigma_min - eta_q*a(6)
     sigma_min = sigma_min - eta_q*a(6);
@@ -501,19 +570,38 @@ if  0 < sigma_min - eta_q*a(6)
     sigma_min = 0;       
 end
 
+C = eye(size(Q));
+
+a = C*[g_q1; g_sig1];
+
+if  0 < sigma_min1 - eta_q*a(6)
+    sigma_min1 = sigma_min1 - eta_q*a(6);
+   
+    elseif 0 <= sigma_min1 - eta_q*a(6)
+    sigma_min1 = 0;       
+end
+
 %  sigma_min = sigma_min - eta_sig*g_sig;
 
 % ------------ ------------ ------------ ------------ ------------ %
 
 
-% ------------ % Keep track of all evolved q_g's % ------------ %
+% ------------ % Keep track of all evolved q_g's with Lambda % ------------ %
 
 q_g(:,1) = q_t;
 q_online(:,1,k) = q_t;
 
+q_g1(:,1) = q_t1;
+q_online1(:,1,k) = q_t1;
+
 % ------------ ------------ ------------ ------------ ------------ %
 
+% ------------ % Keep track of all evolved q_g's without Lambda % ------------ %
 
+q_g2(:,1) = q_t2;
+q_online2(:,1,k) = q_t2;
+
+% ------------ ------------ ------------ ------------ ------------ %
 
 end
 % ------------ % Check feasibility of the answer % ------------ % 
@@ -529,16 +617,16 @@ end
 
 figure (1)
  
-plot(0:k-1,f1)
+plot(0:T-1,f1,'--',0:T-1,f11,0:T-1,f12,':')
 xlabel('$t\,(min)$','Interpreter','latex')
 xlim([0 T-1])
-ylabel('$\tilde{c}_0 f_t(q^g)+\tilde{c}_n\sum\limits_{n \in n_q }\left | q^g \right | - \lambda\sigma_{COMID}$','Interpreter','latex')
+ylabel('$\tilde{c}_0 f_t(q^g)- \lambda\sigma_{COMID}$','Interpreter','latex')%ylabel('$\tilde{c}_0 f_t(q^g)+\tilde{c}_n\sum\limits_{n \in n_q }\left | q^g \right | - \lambda\sigma_{COMID}$','Interpreter','latex')
 x_t = round(T/4,0);
 y_t = f1(x_t,1);
 % txt = ['$\sigma ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f'),'$\,\,\,\,\sigma_{PV} ^ 2 =$',num2str(var_2,'%2.3f')];
-txt = ['$\sigma ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
+txt = ['$\sigma ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta_q =$',num2str(eta_q,'%1.0f')];%txt = ['$\sigma ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
 text(x_t,y_t,txt,'interpreter','latex')
-legend({'COMID(Stochastic)'},'interpreter','latex')
+legend({'$C=Q$','C=I','$Whithot \, \sigma$'},'interpreter','latex')
 
 fig_1 = figure (1);
 cd 'C:\Users\Saeed\OneDrive\UMBC\Dr. Kim\My papers\Matlab\First Paper\Figures_COMD-of-Voltage'
@@ -546,16 +634,17 @@ saveas(fig_1,sprintf('COMID_var_noise=%2.2f_eta=%1.0f_c_n=%2.5f_T=%d_var_PV=%2.3
 cd 'C:\Users\Saeed\OneDrive\UMBC\Dr. Kim\My papers\Matlab\First Paper\OMD-of-Voltage'
 
 figure (2)
-plot(0:k-1,sigma_min_act)
+plot(0:T-1,sigma_min_act,0:T-1,sigma_min_act1,0:T-1,sigma_min_act2)
 xlabel('$t\,(mins)$','Interpreter','latex')
 xlim([0 T-1])
 ylabel('$\sigma_{actual}$','Interpreter','latex')
 x_t = round(T/4,0);
 y_t = sigma_min_act(x_t,1);
 % txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f'),'$\,\,\,\,\sigma_{PV} ^ 2 =$',num2str(var_2,'%2.2f')];
-txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
+txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta_q =$',num2str(eta_q,'%1.0f')];%txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
+
 text(x_t,y_t,txt,'interpreter','latex')
-legend({'\,Minimum \,Singular \.Value \,of \,Jacobian (Actual)'},'interpreter','latex')
+legend({'$C=Q$','C=I','$Whithot \, \sigma$'},'interpreter','latex')
 
 fig_2 = figure (2);
 cd 'C:\Users\Saeed\OneDrive\UMBC\Dr. Kim\My papers\Matlab\First Paper\Figures_COMD-of-Voltage'
@@ -565,141 +654,17 @@ cd 'C:\Users\Saeed\OneDrive\UMBC\Dr. Kim\My papers\Matlab\First Paper\OMD-of-Vol
 
 
 figure (3)
-plot(0:k-1,sigma_min_online)
+plot(0:T-1,sigma_min_online,0:T-1,sigma_min_online1)
 xlabel('$t\,(mins)$','Interpreter','latex')
 xlim([0 T-1])
 ylabel('$\sigma_{online}$','Interpreter','latex')
 x_t = round(T/4,0);
 y_t = sigma_min_act(x_t,1);
 % txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f'),'$\,\,\,\,\sigma_{PV} ^ 2 =$',num2str(var_2,'%2.2f')];
-txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
+txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta_q =$',num2str(eta_q,'%1.0f')];%txt = ['$\sigma_{noise} ^ 2 =$',num2str(var_1,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta_q,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];%
+
 text(x_t,y_t,txt,'interpreter','latex')
-legend({'\,Minimum \,Singular \.Value \,of \,Jacobian (Online)'},'interpreter','latex')
-
-
-
-
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% % ------------ % Deterministic OMD % ------------ %
-% % ------------ % Deterministic OMD % ------------ %
-% % ------------ % Deterministic OMD % ------------ %
-% % ------------ % Deterministic OMD % ------------ %
-% % ------------ % Deterministic OMD % ------------ %
-% 
-% 
-% % ------------ % Initial reactive power for running OMD % ------------ % 
-% q_g = zeros(PV_n,1);
-% % ------------ % Number of periods % ------------ %
-% 
-% 
-% for o = 1:num_real
-% 
-% for k = 1:T
-%     k
-% 
-% [c_d(o,k),~,~,~,~,y2_d(:,k)] = VoltageStability_DF_Solver_vms(nbr,n,r,x,p_c,q_c,p_g,q_g,children,injection_matrix,parent,PV_matrix,PV_n,lambda,zeros(n,1),zeros(n,1),0);
-% f_d(k,1) = 6.6*1000*c_d(o,k) + 6.6*1000*c_n*sum(abs(q_g)); 
-% 
-% % ------------ % The gradient % ------------ %
-% g_d = -y2_d([dual_indeces],k);
-% 
-% % ------------ % Finding q_g for the next step % ------------ %
-% y_d = q_g - eta*g_d;
-% 
-% 
-% for i=1:PV_n
-% 
-% if  q_g_max(i) + eta*c_n < y_d(i)
-%     q_t(i) = q_g_max(i);
-%    
-%     elseif y_d(i) > eta*c_n & y_d(i)<= q_g_max(i) + eta*c_n
-%     q_t(i) = y_d(i) - eta*c_n;
-%     
-%     elseif y_d(i) >= -eta*c_n & y_d(i)<= eta*c_n
-%     q_t(i) = 0;
-%     
-%     elseif y_d(i) >= q_g_min(i) - eta*c_n & y_d(i)< -eta*c_n
-%     q_t(i) = y_d(i) + eta*c_n;
-%     
-%     elseif y_d(i)< q_g_min(i)- eta*c_n
-%     q_t(i) = q_g_min(i);
-%         
-% end
-% end
-% 
-% q_g(:,1) = q_t;
-% 
-% q_det(:,1,k) = q_t;
-% 
-% 
-% end
-% end
-% 
-% 
-% figure (1)
-%  
-% plot(0:k-1,f1,0:k-1,f_d)
-% xlabel('$t\,(min)$','Interpreter','latex')
-% xlim([0 T-1])
-% ylabel('$\tilde{c}_0 E[f_t(q^g)]+\tilde{c}_n\sum\limits_{n \in n_q }\left | q^g \right | $','Interpreter','latex')
-% x_t = round(T/4,0);
-% y_t = f1(x_t,1);
-% txt = ['$\sigma ^ 2 =$',num2str(var,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
-% text(x_t,y_t,txt,'interpreter','latex')
-% legend({'OMD(Stochastic)','OMD(Deterministic)'},'interpreter','latex')
-% 
-% 
-% 
-% %%
-% 
-% 
-% 
-%  [c2,~, ~,~,~,~,~,q_g_2] = Optimal_DistFlowSolver_vms_q_g(nbr,n,PV_n,r,x,p_c,q_c,p_g,q_g,children,injection_matrix,parent,PV_matrix,q_g_max,zeros(n,1),zeros(n,1),0);
-% 
-%  [c3,~, ~,~,~,~,~,q_g_3] = Optimal_DistFlowSolver_vms(nbr,n,PV_n,r,x,p_c,q_c,p_g,q_g,children,injection_matrix,parent,PV_matrix,zeros(n,1),zeros(n,1),0);
-% 
-%  
-%  f2 = 6.6*1000*c2+ 6.6*1000*c_n*sum(abs(q_g_2)); 
-%  
-%  f3 = 6.6*1000*c3 + 6.6*1000*c_n*sum(abs(q_g_3)); 
-%  
-% figure (2)
-%  
-% plot(0:k-1,f1,0:k-1,f_d,0:k-1,f2*ones(1,T),0:k-1,f3*ones(1,T))
-% xlabel('$t\,(min)$','Interpreter','latex')
-% xlim([0 T-1])
-% ylabel('$\tilde{c}_0 E[f_t(q^g)]+\tilde{c}_n\sum\limits_{n \in n_q }\left | q^g \right | $','Interpreter','latex')
-% x_t = round(T/4,0);
-% y_t = f1(x_t,1);
-% txt = ['$\sigma ^ 2 =$',num2str(var,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
-% text(x_t,y_t,txt,'interpreter','latex')
-% legend({'OMD(Stochastic)','OMD(Deterministic)','Convex Problem','Unconstrained $q^g$'},'interpreter','latex')
-% 
-% figure (3)
-% 
-% plot(0:k-1,c0,0:k-1,c_d,0:k-1,c2*ones(1,T),0:k-1,c3*ones(1,T))
-% xlabel('$t\,(min)$','Interpreter','latex')
-% xlim([0 T-1])
-% ylabel('$E[f_t(q^g)]$','Interpreter','latex')
-% x_t = round(T/4,0);
-% y_t = c0(x_t);
-% txt = ['$\sigma ^ 2 =$',num2str(var,'%2.2f'),'$\,\,\,\,\eta =$',num2str(eta,'%1.0f'),'$\,\,\,\,c_n =$',num2str(c_n,'%2.5f')];
-% text(x_t,y_t,txt,'interpreter','latex')
-% legend({'OMD(Stochastic)','OMD(Deterministic)','Convex Problem','Unconstrained $q^g$'},'interpreter','latex')
-%  
-% 
-
-
-
-%%
-
+legend({'$C=Q$','C=I'},'interpreter','latex')
 
 
 
